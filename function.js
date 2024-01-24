a640c5e2cc2dbc6cac39d24ab448ded604014456
Daniel <streetbossog@gmail.com>
2024-01-24 19:15:51 +0300

    main

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 let a = 3
-
+// sayHi() **
 function mult(x,y){
     // y = y || 6
     y = y ? y : 6
@@ -14,3 +14,7 @@ function myFunc(){
 let b = mult(a)
 
 console.log(b)
+
+// const sayHi = function(){ this function can't be used earlier, because it's a variable **
+
+// }
