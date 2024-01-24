15a689cbec6569fbe2e1cfed3d0413febe6c5ab2
Daniel <streetbossog@gmail.com>
2024-01-24 20:21:28 +0300

    main

--- function.js
+++ function.js
@@ -14,40 +14,49 @@
 // }
 // alert(sum(3,5))
 
-counter = 0
-function hi(name, text="Привет") {
-    counter++;
-    alert(name+ ", " +text)
-}
- function showHiCount() { //declaration function can be used before it was created
-    alert("Я поприветствовал " + counter + " человек")
-}
-hi("Вася")
-hi("Петя")
-hi("Настя", "здравствуйте")
-showHiCount()
-let a = 3
+// counter = 0
+// function hi(name, text="Привет") {
+//     counter++;
+//     alert(name+ ", " +text)
+// }
+//  function showHiCount() { //declaration function can be used before it was created
+//     alert("Я поприветствовал " + counter + " человек")
+// }
+// hi("Вася")
+// hi("Петя")
+// hi("Настя", "здравствуйте")
+// showHiCount()
+// let a = 3
 // sayHi() **
-function mult(x,y){
-    // y = y || 6
-    y = y ? y : 6
-    a = x * 2 + y
-    return a  
+// function mult(x,y){
+//     // y = y || 6
+//     y = y ? y : 6
+//     a = x * 2 + y
+//     return a  
+// }
+
+
+
+// function myFunc(){
+//     return 2 * 2
+// }
+
+//  const sayHi = function(callbackOne, callbackTwo){ // this function can't be used earlier, because it's a variable 
+//  //** expression function  be used before it was created //
+//     console.log(callbackOne(3))
+//     console.log(callbackTwo())
+// // 
+//  }
+//  let b = mult(a)
+
+// //  mult = callbackOne
+// //  myFunc = callbackTwo
+
+//  sayHi(mult,myFunc)
+
+let a = 3
+let mult = (x, y = 6) => {
+a = x * 2 + y
+    return a
 }
-
-function myFunc(){
-    return 2 * 2
-}
-
- const sayHi = function(callbackOne, callbackTwo){ // this function can't be used earlier, because it's a variable 
- //** expression function  be used before it was created //
-    console.log(callbackOne(3))
-    console.log(callbackTwo())
-// 
- }
- let b = mult(a)
-
-//  mult = callbackOne
-//  myFunc = callbackTwo
-
- sayHi(mult,myFunc)
+console.log(mult(a))
