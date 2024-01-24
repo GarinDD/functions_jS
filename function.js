88cdb657fbb520be524dcc5647d8bc159ef2a9d7
Daniel <streetbossog@gmail.com>
2024-01-24 19:21:29 +0300

    main

--- function.js
+++ function.js
@@ -1,3 +1,31 @@
+// function createRandom() {
+//    return Math.round(Math.random() * 6) + 1;
+// }
+// x = createRandom();
+// alert(x)
+
+// for(i=i;i<5;i++) {
+//     // console.log(createRandom())
+// }
+
+// function sum(a, b) {
+//     console.log( a +b )
+//     return a +b ;
+// }
+// alert(sum(3,5))
+
+counter = 0
+function hi(name, text="Привет") {
+    counter++;
+    alert(name+ ", " +text)
+}
+function showHiCount() {
+    alert("Я поприветствовал " + counter + " человек")
+}
+hi("Вася")
+hi("Петя")
+hi("Настя", "здравствуйте")
+showHiCount()
 let a = 3
 // sayHi() **
 function mult(x,y){
